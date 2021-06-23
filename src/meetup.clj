a9a4327a3ae895e42d5a64e00c05b85be18d69f5
Hiram MADELAINE <hiram.madelaine@gmail.com>
2021-06-23 08:43:12 +0200

    Init

---
+++ src/meetup.clj
@@ -0,0 +1,154 @@
+(ns meetup)
+
+;┌──────────────────────────────────┐
+;│                                  │
+;│      Les données littérales      │
+;│                                  │
+;└──────────────────────────────────┘
+
+
+; Chaîne de caractères
+
+"Hello world"
+
+"Je suis une chaîne de caractères
+sur plusieurs lignes"
+
+;Expressions régulières
+
+#"^Clojure$"
+
+
+; Les nombres
+
+1
+1.23
+1.23M
+12322323280327983929N
+1/5
+
+;┌──────────────────────────────────┐
+;│                                  │
+;│         Les collections          │
+;│                                  │
+;└──────────────────────────────────┘
+
+;List
+'(1 2 3)
+
+; Vector
+[1 2 3]
+
+; Map
+{:valeur 1.23M
+ :devise :EUR}
+
+; Set
+#{1 2 3}
+#{1 2 3 3}
+
+{:valeurs [{:valeur 1.23M
+            :devise :EUR}
+           {:valeur 1.74M
+            :devise :EUR}]}
+
+;┌──────────────────────────────────┐
+;│                                  │
+;│         Les fonctions !          │
+;│                                  │
+;└──────────────────────────────────┘
+
+
+(+ 1 2)
+
+(* 10 6)
+
+(* 3 (+ 1 3))
+
+(/ 2 3)
+
+(< 1 2)
+
+(odd? 1)
+
+(even? 1)
+
+
+
+; La notation préfixées a un avantage
+
+
+(+ 1 2 3 4 5 6 7 8 9)
+
+(< 1 3 2)
+
+
+;┌──────────────────────────────────┐
+;│                                  │
+;│    Les fonctions usuelles !      │
+;│                                  │
+;└──────────────────────────────────┘
+
+; Elles prennent toutes une autre fonction en parametres
+
+(map inc [1 2 3])
+
+(filter odd? '(1 2 3))
+
+(remove even? [1 2 3 4])
+
+
+
+(reduce + [1 2 3 4 5 6 7 8 9])
+
+(reductions + [1 2 3 4 5 6 7 8 9])
+(+ (+ (+ (+ (+ (+ (+ (+ 1 2) 3) 4) 5) 6) 7) 8) 9)
+
+;(+ 1 2 3 4 5 6 7 8 9)
+
+(reduce + 100 [1 2 3 4 5 6 7 8 9])
+
+(str "a" "b")
+
+(reduce str [\H \e \l \l \o \space \C \l \o \j \u \r \e])
+
+
+(range 10)
+
+(reduce * (filter odd? (map inc (range 10))))
+
+; Partial / Curry
+(def plus-2 (partial + 2))
+
+(plus-2 2)
+
+;Closure
+(defn plus
+  [x]
+  (fn [y]
+    (+ x y)))
+
+((plus 8) 2)
+
+
+
+
+(def infini (range))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
